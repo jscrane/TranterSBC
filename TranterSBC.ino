e0d6e1f5bc18d3c264f26f3b06a0af15a94cc54d
steve <jscrane@gmail.com>
2026-03-10 10:44:14 +0000

    ACIA and VIA are Memory::Device once more

--- TranterSBC.ino
+++ TranterSBC.ino
@@ -8,87 +8,65 @@
 Memory memory;
 r6502 cpu(memory);
 Arduino machine(cpu);
+ACIA acia;
+VIA via;
+prom basic(osi_bas, 16384);
+ram<> pages[32];
 
 static void irq_handler(bool irq) {
 	if (irq)
 		cpu.raise(0);
 }
 
-class SerialAcia: public Memory::Device {
-public:
-	SerialAcia(): Memory::Device(8192) {}
-
-	void init() {
-		_acia.register_framing_handler([](uint8_t) {
+static void acia_init() {
+	acia.register_framing_handler([](uint8_t) {
 #if DEBUGGING == DEBUG_NONE
-			Serial.begin(TERMINAL_SPEED, SERIAL_8N1);
+		Serial.begin(TERMINAL_SPEED, SERIAL_8N1);
 #endif
-		});
-		_acia.register_read_data_handler([]() {
-			uint8_t b = Serial.read();
-			DBG_EMU("read: %02x", b);
-			if (b == 0x0e)		// ^N
-				machine.reset();
-			else if (b == 0x08)	// BS
-				b = '_';
-			return b;
-		});
-		_acia.register_write_data_handler([](uint8_t b) {
-			DBG_EMU("write: %02x", b);
-			if (b == '_') {
-				Serial.write(0x08);
-				Serial.write(' ');
-				Serial.write(0x08);
-				return;
-			}
-			Serial.write(b);
-		});
-		_acia.register_can_rw_handler([](void) {
-			uint8_t s = 0;
-			if (Serial.available() > 0) s++;
-			if (Serial.availableForWrite() > 0) s += 2;
-			DBG_EMU("can_rw: %x", s);
-			return s;
-		});
-		_acia.register_irq_handler(irq_handler);
-		machine.register_pollable(_acia);
-	}
-
-	virtual void operator=(uint8_t b) { _acia.write(_acc, b); }
-	virtual operator uint8_t() { return _acia.read(_acc); }
-
-private:
-	ACIA _acia;
-
-} acia;
+	});
+	acia.register_read_data_handler([]() {
+		uint8_t b = Serial.read();
+		DBG_EMU("read: %02x", b);
+		if (b == 0x0e)		// ^N
+			machine.reset();
+		else if (b == 0x08)	// BS
+			b = '_';
+		return b;
+	});
+	acia.register_write_data_handler([](uint8_t b) {
+		DBG_EMU("write: %02x", b);
+		if (b == '_') {
+			Serial.write(0x08);
+			Serial.write(' ');
+			Serial.write(0x08);
+			return;
+		}
+		Serial.write(b);
+	});
+	acia.register_can_rw_handler([](void) {
+		uint8_t s = 0;
+		if (Serial.available() > 0) s++;
+		if (Serial.availableForWrite() > 0) s += 2;
+		DBG_EMU("can_rw: %x", s);
+		return s;
+	});
+	acia.register_irq_handler(irq_handler);
+	machine.register_pollable(acia);
+}
 
 // Lilygo TTGO
 #define BUTTON_PIN	36
 #define LED_PIN		14
 
-class ViaDevice: public Memory::Device {
-public:
-	ViaDevice(): Memory::Device(8192) {}
-
-	void init() {
-		_via.register_irq_handler(irq_handler);
-		_via.register_porta_input_handler([]() {
-			return (uint8_t)!digitalRead(BUTTON_PIN);
-		});
-		_via.register_porta_output_handler([](uint8_t b) {
-			digitalWrite(LED_PIN, (b & 0x80)? LOW: HIGH);
-		});
-	}
-
-	virtual void operator=(uint8_t b) { _via.write(_acc, b); }
-	virtual operator uint8_t() { return _via.read(_acc); }
-
-private:
-	VIA _via;
-} via;
-
-prom basic(osi_bas, 16384);
-ram<> pages[32];
+static void via_init() {
+	via.register_irq_handler(irq_handler);
+	via.register_porta_input_handler([]() {
+		return (uint8_t)!digitalRead(BUTTON_PIN);
+	});
+	via.register_porta_output_handler([](uint8_t b) {
+		digitalWrite(LED_PIN, (b & 0x80)? LOW: HIGH);
+	});
+}
 
 void setup() {
 
@@ -102,12 +80,12 @@ void setup() {
 	for (unsigned i = 0; i < 32; i++)
 		memory.put(pages[i], i * ram<>::page_size);
 
-	memory.put(via, 0x8000);
-	memory.put(acia, 0xa000);
+	memory.put(via, 0x8000, 8192);
+	memory.put(acia, 0xa000, 8192);
         memory.put(basic, 0xc000);
 
-	via.init();
-	acia.init();
+	via_init();
+	acia_init();
 
 	machine.reset();
 }
